777e942a873de8b482be2b74fd7b46b4a9b2e48b
Trishul-creator <gtrish2010@gmail.com>
2024-12-31 19:27:22 -0600

    Update motor configuration and project settings for improved drive control

--- src/RobotConfig.cpp
+++ src/RobotConfig.cpp
@@ -2,13 +2,13 @@
 #include "vex.h"
 
 inertial BrainInertial = inertial();
-motor LeftDriveSmart = motor(PORT4, 1, false);
-motor RightDriveSmart = motor(PORT3, 1, true);
+motor LeftDriveSmart = motor(PORT3, 1, false);
+motor RightDriveSmart = motor(PORT9, 1, true);
 smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, BrainInertial, 200);
 brain Brain;
-motor LauncherMotorA = motor(PORT1, false);
-motor LauncherMotorB = motor(PORT6, true);
-motor_group Launcher = motor_group(LauncherMotorA, LauncherMotorB);
-motor IntakeMotor = motor(PORT2, true);
-motor BackRoller = motor(PORT5, true);
+motor Launcher = motor(PORT7, true);
+motor IntakeMotorA = motor(PORT5, true);
+motor IntakeMotorB = motor(PORT6, false);
+motor_group IntakeMotor = motor_group(IntakeMotorA, IntakeMotorB);
+motor BackRoller = motor(PORT1, true);
 PIDController pid = PIDController(0.5, 0.0, 0.0, 0.5, 0.0, 0.0);
